19983d69c180cc6d70598994e2b1068593155119
SaitoMMR <162839577+SaitoMMR@users.noreply.github.com>
2024-06-07 10:59:05 +0900

    kadai_007ディレクトリの追加

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>kadai_007</title>
+</head>
+<body>
+  <p>
+    <?php
+    $arrays = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+    echo '<br>';
+    print_r($arrays);
+    ?>
+  </P>
+</body>
+</html>
